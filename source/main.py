7a3ab45daf868cbd0a7e721e192e49e6521708f8
aut0wash <jules.mahe@gmail.com>
2020-11-20 23:12:39 +0100

    secret key savec in secret.py

--- source/main.py
+++ source/main.py
@@ -10,6 +10,8 @@ import os
 import logging
 import utils
 
+from secret import TOKEN
+
 auto_id = 154428278302703616
 kuaj_id = 189489874645155841
 mob_id = 198530702558494720
@@ -18,20 +20,22 @@ bot_ids = [728962844158328883, 453117389802831882]
 
 authorized_ids = [auto_id, kuaj_id, mob_id]
 
-token = sys.argv[1]
+token = TOKEN
 
-description = "Soundboard des Cons"
+description = 'Soundboard des Cons'
+#intents = discord.Intents.default()
+#intents.members = True  # Subscribe to the privileged members intent.
+#client = commands.Bot(command_prefix='!', intents=intents, description=description)
 
-intents = discord.Intents.default()
-intents.members = True  # Subscribe to the privileged members intent.
-client = commands.Bot(command_prefix='!', intents=intents, description=description)
 
-server_name = "Le Think Tank"
-default_role = 295466109698703362  # Complotiste
+client = commands.Bot(command_prefix='!', description=description)
+
+server_name = 'Le Discord des Cons'
+default_role = "Trou du cul la balayette"
 
 
 def setup_logging():
-    logging.getLogger("discord").setLevel(logging.WARNING)
+    logging.getLogger('discord').setLevel(logging.WARNING)
 
     root = logging.getLogger()
     root.setLevel(logging.INFO)
@@ -39,7 +43,7 @@ def setup_logging():
     handler = logging.StreamHandler(sys.stdout)
     handler.setLevel(logging.DEBUG)
     formatter = logging.Formatter(
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
     root.addHandler(handler)
 
@@ -54,9 +58,8 @@ class UnAuthorized(Exception):
 
 def check_auth(message):
     if not message.author.id in authorized_ids:
-        logging.warning(
-            f"{message.author.id} - {message.author.name} has tried a command with insufficient permissions")
-        raise UnAuthorized("This command requires privileged rights")
+        logging.warning(f"{message.author.id} - {message.author.name} has tried a command with insufficient permissions")
+        raise UnAuthorized('This command requires privileged rights')
 
 
 def is_admin():
@@ -70,29 +73,24 @@ async def on_ready():
     try:
         client.general_chan = discord.utils.get(
             client.get_all_channels(), guild__name=server_name, name="general")
+        client.test_chan = client.get_channel(239407287091986432)
         client.guild = client.get_guild(198534713575473152)
+
         client.samples = utils.load_json("samples.json")
-        # link = await client.general_chan.create_invite(max_age=0, max_uses=1, unique=True)
-        # logging.info(f"Invitation link: {link}")
-        role = discord.utils.get(client.guild.roles, id=default_role)
-        logging.info(
-            f"Checking that everyone has at least the default role {role}")
-        for member in client.guild.members:
-            if len(member.roles) == 1:
-                await member.add_roles(role)
-                logging.info(f"Added role {role} for {member.display_name}")
+
         logging.info("Ready !")
     except Exception as e:
-        logging.error(f"Error in on_ready: {e}")
+        logging.error('Error in on_ready: {}'.format(e))
 
 
 @client.event
 async def on_member_join(member):
     try:
-        await member.add_roles(discord.utils.get(member.guild.roles, id=default_role))
+        role = discord.utils.get(member.guild.roles, name=default_role)
+        await member.add_roles(role)
         logging.info("Added role for new member")
     except Exception as e:
-        logging.error(f"Error in on_member_join: {e}")
+        logging.error('Error in on_member_join: {}'.format(e))
 
 
 @client.event
@@ -100,7 +98,7 @@ async def on_reaction_add(reaction, user):
     try:
         pass
     except Exception as e:
-        logging.error(f"Error in on_reaction_add: {e}")
+        logging.error('Error in on_reaction_add: {}'.format(e))
 
 
 @client.event
@@ -108,14 +106,14 @@ async def on_voice_state_update(member, before, after):
     try:
         pass
     except Exception as e:
-        logging.error(f"Error in on_voice_state_update: {e}")
+        logging.error('Error in on_voice_state_update: {}'.format(e))
 
 if __name__ == "__main__":
     try:
         setup_logging()
-        for filename in os.listdir("./cogs"):
-            if filename.endswith(".py"):
-                client.load_extension(f"cogs.{filename[:-3]}")
+        for filename in os.listdir('./cogs'):
+            if filename.endswith('.py'):
+                client.load_extension(f'cogs.{filename[:-3]}')
         client.run(token)
     except Exception as e:
-        logging.error(f"Error in __main__: {e}")
+        logging.error('Error in __main__: {}'.format(e))
